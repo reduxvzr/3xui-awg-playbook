dc74b4696ec0805c0689d0a684dbc0d7d022db98
Roman Chervyakov <roman.chervyakov@travelata.ru>
2026-03-08 10:53:40 +0300

    change dnscrypt version; create crontab; local subnet for dnscrypt

--- 3xui-installation/files/dnscrypt-proxy/get_filters.sh
+++ 3xui-installation/files/dnscrypt-proxy/get_filters.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 filter_path=/opt/dnscrypt-proxy/filters
 
-curl -so $filter_path/ruAdguard.txt https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_1_Russian/filter.txt
-curl -so $filter_path/shakal.txt https://schakal.ru/hosts/hosts.txt
-curl -so $filter_path/stevenBlack.txt https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+curl --max-time 25 -so $filter_path/ruAdguard.txt https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_1_Russian/filter.txt
+curl --max-time 25 -so $filter_path/shakal.txt https://schakal.ru/hosts/hosts.txt
+curl --max-time 25 -so $filter_path/stevenBlack.txt https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 
 cat $filter_path/ruAdguard.txt $filter_path/shakal.txt $filter_path/stevenBlack.txt > $filter_path/blocked-names.txt
 rm -f $filter_path/ruAdguard.txt $filter_path/shakal.txt $filter_path/stevenBlack.txt
